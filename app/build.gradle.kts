0c512bc4eb27850a8cc0e6d9ba2af667e33de6fe
BakonJarser <github@chewbarker.net>
2024-02-28 18:52:45 -0800

    Update from Groovy to Kotlin for build files.  Update room and navigation libraries.

---
+++ app/build.gradle.kts
@@ -0,0 +1,79 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt")
+    id("com.google.devtools.ksp")
+    id("androidx.navigation.safeargs.kotlin")
+}
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.cellblock70.popularmovies"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 2
+        versionName = "2"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    buildTypes.all {
+        // This assumes TmdbApiKey is defined elsewhere in your gradle.properties or as an environment variable
+        buildConfigField("String", "TMDB_MAP_API_KEY", "\"${findProperty("TmdbApiKey") ?: ""}\"")
+    }
+
+    buildFeatures {
+        dataBinding = true
+    }
+
+    namespace = "com.cellblock70.popularmovies"
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1") {
+        exclude(group = "com.android.support", module = "support-annotations")
+    }
+    testImplementation("junit:junit:4.13.2")
+
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.vectordrawable:vectordrawable:1.1.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    api("com.google.android.material:material:1.11.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+
+    // Room database
+    val roomVersion = "2.6.1"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("com.squareup.moshi:moshi:1.15.1")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.1")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.12")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+    implementation("com.jakewharton.timber:timber:5.0.1")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+
+    /**
+     * Use `apply false` in the top-level build.gradle file to add a Gradle
+     * plugin as a build dependency but not apply it to the current (root)
+     * project. Don"t use `apply false` in sub-projects. For more information,
+     * see Applying external plugins with same version to subprojects.
+     */
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+    id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
+
+}
+
+buildscript {
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.7")
+
+
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
