cb99d00f323216996989dd5bffe5e757a9f8ab71
BakonJarser <github@chewbarker.net>
2025-06-30 10:51:29 -0700

    Library updates.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,36 +56,37 @@ dependencies {
     }
     testImplementation("junit:junit:4.13.2")
 
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.7.8")
+    implementation("androidx.core:core-ktx:1.16.0")
 
     // Room database
-    val roomVersion = "2.6.1"
+    val roomVersion = "2.7.2"
     implementation("androidx.room:room-runtime:$roomVersion")
     implementation("androidx.room:room-ktx:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
 
-    implementation("androidx.core:core-ktx:1.15.0")
     implementation("com.squareup.moshi:moshi:1.15.2")
     implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
-    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.14")
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-moshi:3.0.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.16")
     implementation("com.jakewharton.timber:timber:5.0.1")
 
     // Compose
-    implementation("androidx.compose.ui:ui:1.7.8")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("androidx.compose.material3:material3:1.3.1")
-    implementation("androidx.compose.ui:ui-graphics:1.7.8")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.7.8")
-    implementation("androidx.compose.foundation:foundation:1.7.8")
-    implementation("androidx.compose.runtime:runtime:1.7.8")
+    val composeVersion = "1.8.3"
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.ui:ui-graphics:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    implementation("androidx.compose.foundation:foundation:$composeVersion")
+    implementation("androidx.compose.runtime:runtime:$composeVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.9.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.1")
+    implementation("androidx.compose.material3:material3:1.3.2")
     implementation("androidx.activity:activity-compose:1.10.1")
-    implementation("androidx.navigation:navigation-compose:2.8.9")
-    implementation("io.coil-kt.coil3:coil-compose:3.1.0")
-    implementation("io.coil-kt.coil3:coil-network-okhttp:3.1.0")
+    implementation("androidx.navigation:navigation-compose:2.9.0")
+    implementation("io.coil-kt.coil3:coil-compose:3.2.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.2.0")
     implementation("androidx.compose.compiler:compiler:1.5.15")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.7.3")
 
